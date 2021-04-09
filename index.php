c6cdb985c0494a64b963226b9e92b5fea9e63288
alexandre maciel <alemaciell@gmail.com>
2021-04-08 20:28:22 -0400

    ajustes iniciais

--- index.php
+++ index.php
@@ -8,50 +8,65 @@
                     <h3>Gostaria de conhecer o Camboja?</h3>
                     <p>Baixe Grátis nosso roteiro. Guia incrível para bares, restaurantes, hotéis e passeios.</p>
                 </div>
+                <hr class="hidden-lg hidden-md space">
                 <div class="col-md-2">
-                    <a href="#" class="btn btn-outline-primary">Baixe o Roteiro</a>
+                    <a href="#" class="btn btn-lg btn-outline-primary">Baixe o Roteiro</a>
                 </div>
             </div>
         </div>
 
         <div class="row mt-5">
             <div class="col-md-12">
-                <h1>Para onde você quer ir?</h1>
+                <h2 class="title">Para onde você quer ir?</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, error atque nobis ratione quos porro illo? Provident, illo, laborum impedit accusamus obcaecati aperiam, beatae doloremque quaerat eos dolore deserunt excepturi?</p>
             </div>
         </div>
 
         <div class="row mt-2">
-            <div class="col-md-3">
+            <div class="col-md-3 col-6">
                 <div class="card">
-                    <img src="https://via.placeholder.com/270x370" alt="" class="card-img">
-                    <div class="card-img-overlay">
-                        <h4>África e Ilhas</h4>
-                    </div>
+                    <!-- 270x370 -->
+                    <a href="#">
+                        <img src="img/onde_1.png" alt="" class="card-img">
+                        <div class="card-img-overlay">
+                            <h2 class="sm-title">África e Ilhas</h2>
+                        </div>
+                    </a>
                 </div>
             </div>
-            <div class="col-md-3">
+
+            <div class="col-md-3 col-6">
                 <div class="card">
-                    <img src="https://via.placeholder.com/270x370" alt="" class="card-img">
-                    <div class="card-img-overlay">
-                        <h4>América Central e Caríbe</h4>
-                    </div>
+                    <a href="#">
+                        <img src="img/onde_2.png" alt="" class="card-img">
+                        <div class="card-img-overlay">
+                            <h2 class="sm-title">América Central e Caríbe</h2>
+                        </div>
+                    </a>
                 </div>
             </div>
-            <div class="col-md-3">
+
+            <hr class="hidden-lg hidden-md space">
+
+            <div class="col-md-3 col-6">
                 <div class="card">
-                    <img src="https://via.placeholder.com/270x370" alt="" class="card-img">
-                    <div class="card-img-overlay">
-                        <h4>América do Norte</h4>
-                    </div>
+                    <a href="#">
+                        <img src="img/onde_3.png" alt="" class="card-img">
+                        <div class="card-img-overlay">
+                            <h2 class="sm-title">América do Norte</h2>
+                        </div>
+                    </a>
                 </div>
             </div>
-            <div class="col-md-3">
+
+            <div class="col-md-3 col-6">
                 <div class="card">
-                    <img src="https://via.placeholder.com/270x370" alt="" class="card-img">
-                    <div class="card-img-overlay">
-                        <h4>América do Sul</h4>
-                    </div>
+                    <a href="#">
+                        <img src="img/onde_4.png" alt="" class="card-img">
+                        <div class="card-img-overlay">
+                            <h2 class="sm-title">América do Sul</h2>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
@@ -59,46 +74,62 @@
         <div class="row mt-4">
             <div class="col-md-12">
                 <div class="card">
-                    <img src="https://via.placeholder.com/1170x375" alt="" class="card-img">
-                    <div class="card-img-overlay">
-                        <h2>Tailândia</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quo minus quasi inventore, dolor facere reprehenderit. Unde sit natus temporibus nihil nisi non? Recusandae, iste alias. Quasi excepturi porro maxime.</p>
-                    </div>
+                    <!-- 1170x375 -->
+                    <a href="#">
+                        <img src="img/tailandia.png" alt="" class="card-img">
+                        <div class="card-img-overlay">
+                            <h2 class="lg-title">Tailândia</h2>
+                            <p class="hide-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quo minus quasi inventore, dolor facere reprehenderit. Unde sit natus temporibus nihil nisi non? Recusandae, iste alias. Quasi excepturi porro maxime.</p>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
 
         <div class="row mt-5">
             <div class="col-md-12">
-                <h1>A Ocasião faz o destino</h1>
+                <h2 class="title">A Ocasião faz o destino</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, error atque nobis ratione quos porro illo? Provident, illo, laborum impedit accusamus obcaecati aperiam, beatae doloremque quaerat eos dolore deserunt excepturi?</p>
             </div>
         </div>
 
         <div class="row mt-4">
-            <div class="col-md-2">
-                <img src="https://via.placeholder.com/170x170" class="img-thumbnail mb-3" alt="">
-                <p class="txt-thumbnail">Casamento</p>
+            <div class="col-md-2 col-6">
+                <!-- 170x170 -->
+                <a href="#">
+                    <img src="img/ocasiao/ocasiao1.png" class="img-thumbnail mb-3" alt="">
+                    <p class="txt-thumbnail">Casamento</p>
+                </a>
             </div>
-            <div class="col-md-2">
-                <img src="https://via.placeholder.com/170x170" class="img-thumbnail mb-3" alt="">
-                <p class="txt-thumbnail">Aniversário</p>
+            <div class="col-md-2 col-6">
+                <a href="#">
+                    <img src="img/ocasiao/ocasiao2.png" class="img-thumbnail mb-3" alt="">
+                    <p class="txt-thumbnail">Aniversário</p>
+                </a>
             </div>
-            <div class="col-md-2">
-                <img src="https://via.placeholder.com/170x170" class="img-thumbnail mb-3" alt="">
-                <p class="txt-thumbnail">Conquistas</p>
+            <div class="col-md-2 col-6">
+                <a href="#">
+                    <img src="img/ocasiao/ocasiao3.png" class="img-thumbnail mb-3" alt="">
+                    <p class="txt-thumbnail">Conquistas</p>
+                </a>
             </div>
-            <div class="col-md-2">
-                <img src="https://via.placeholder.com/170x170" class="img-thumbnail mb-3" alt="">
-                <p class="txt-thumbnail">Formatura</p>
+            <div class="col-md-2 col-6">
+                <a href="#">
+                    <img src="img/ocasiao/ocasiao4.png" class="img-thumbnail mb-3" alt="">
+                    <p class="txt-thumbnail">Formatura</p>
+                </a>
             </div>
-            <div class="col-md-2">
-                <img src="https://via.placeholder.com/170x170" class="img-thumbnail mb-3" alt="">
-                <p class="txt-thumbnail">Celebração</p>
+            <div class="col-md-2 col-6">
+                <a href="#">
+                    <img src="img/ocasiao/ocasiao5.png" class="img-thumbnail mb-3" alt="">
+                    <p class="txt-thumbnail">Celebração</p>
+                </a>
             </div>
-            <div class="col-md-2">
-                <img src="https://via.placeholder.com/170x170" class="img-thumbnail mb-3" alt="">
-                <p class="txt-thumbnail">Bodas</p>
+            <div class="col-md-2 col-6">
+                <a href="#">
+                    <img src="img/ocasiao/ocasiao6.png" class="img-thumbnail mb-3" alt="">
+                    <p class="txt-thumbnail">Bodas</p>
+                </a>
             </div>
         </div>
     </div>
@@ -106,8 +137,16 @@
 
 <div class="session session-inverted">
     <div class="container">
-        <h1>Em dev</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, error atque nobis ratione quos porro illo? Provident, illo, laborum impedit accusamus obcaecati aperiam, beatae doloremque quaerat eos dolore deserunt excepturi?</p>
+        <div class="row">
+            <div class="col-md-6">
+                <h2 class="title">Depoimentos <br>de quem Curtiu</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non nulla a augue mollis dapibus. Proin accumsan, tortor vitae commodo</p>
+            </div>
+            <div class="col-md-6 hide-md text-right">
+                <img src="img/aspas.png" alt="">
+            </div>
+        </div>
+        
     </div>
 </div>
 
